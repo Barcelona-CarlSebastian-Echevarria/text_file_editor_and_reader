94a95ba18991c3c25d8c8cef80a6ab6eaf512d04
Carl <carlsebpogi123@gmail.com>
2024-11-28 20:35:28 +0800

    Pseudocode upload

---
+++ text_file_editor.py
@@ -0,0 +1,8 @@
+# Create a dedicated txt file for the user; name of file is of user's desire
+# Upon file creation, profile the user with essential questions; the output shall be written in the file
+# Give the user the privilege to input more information; otherwise, give the user option to exit
+# Format the inputs in the txt file-- make it visually appealing as much as possible
+# Draft: Ask the user whether they want to create another file for another profiling
+# Draft: Store each file name in a list for navigation and latter edits
+
+
