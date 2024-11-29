421237ffbbd2587245e1e998be2df4b6ce5f469a
Carl <carlsebpogi123@gmail.com>
2024-11-29 21:36:02 +0800

    Pseudocode upload for text reader

---
+++ text_file_reader.py
@@ -0,0 +1,3 @@
+# Use the data in the text_file_editor.py to direct the program on which row it should start to read
+# Projectedly, use indices and list to guide the user what information is entered 
+# If the user input match a value, use its index to print the row it belong to
