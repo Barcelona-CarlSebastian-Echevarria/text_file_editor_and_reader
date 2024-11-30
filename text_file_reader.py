ae979d68bde40935292e0501fbaf79ed122553f6
Carl <carlsebpogi123@gmail.com>
2024-11-30 15:31:11 +0800

    Comments not uploaded in the last commit

--- text_file_reader.py
+++ text_file_reader.py
@@ -2,13 +2,18 @@
 # Projectedly, use indices and list to guide the user what information is entered 
 # If the user input match a value, use its index to print the row it belong to
 
+
+# Links the output of text editor with reader
+# This block should be constant, unless the program's settings are to be modified
 with open("text_file_editor.txt", "r") as file:
     filter_list = []
     for line in file:
+      # Removes the spaces between lines
       line = line.strip()
       filter_list.append(line)
 
 file_path = filter_list[2]
 name_list = filter_list[4:]
 print(file_path)
+# Not yet working
 print(name_list)
